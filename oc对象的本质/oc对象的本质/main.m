3b9ce79f8060a3aa78758b84ee199b54f33985fe
chenl326 <347979248@qq.com>
2022-06-04 16:55:14 +0800

    pull

--- oc对象的本质/oc对象的本质/main.m
+++ oc对象的本质/oc对象的本质/main.m
@@ -15,6 +15,7 @@ int main(int argc, char * argv[]) {
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
         
         NSObject *obj = [[NSObject alloc]init];
+        NSLog(@"obj");
         
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
